fe3b8b29c34c6564fc4b26da1a725fe040a051f6
thecadeharp <cadeharper513@gmail.com>
2026-09-23 11:56:59 -0400

    Update portfolio-config.js

--- portfolio-config.js
+++ portfolio-config.js
@@ -1,5 +1,5 @@
 window.THI_PORTFOLIO_CONFIG = {
-  enabled: false,
-  url: "",
-  publishableKey: ""
+  enabled: true,
+  url: "https://wfpdoygrmojtkmcqzceb.supabase.co",
+  publishableKey: "sb_publishable_to8a66HCsJVJjoR_R1QYsg_7Y0to02b"
 };
